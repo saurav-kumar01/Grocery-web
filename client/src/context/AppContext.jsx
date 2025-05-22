f33d18300b52b6986ed51a9b9f9c0a2033b9268c
Saurav Kumar <saurabhmahur1098@gmail.com>
2025-05-23 02:27:02 +0530

    Update Login and SignUp section and add Products section

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -14,6 +14,7 @@ export const AppContextProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
 
   const [cartItems, setCartItems] = useState({});
+  const [searchQuery, setSearchQuery] = useState({});
 
   // fetch all products
   const fetchProducts = async () => {
@@ -73,6 +74,8 @@ export const AppContextProvider = ({ children }) => {
     updateCartItem,
     removeFromCart,
     cartItems,
+    searchQuery,
+    setSearchQuery,
   };
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
